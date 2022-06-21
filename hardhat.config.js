f53e6251b25208174281e4fe8fc2e8663ac85e95
Blaine Bublitz <blaine.bublitz@gmail.com>
2022-06-20 17:26:42 -0700

    feat: Implement testing using hardhat-circom utilities (#8)
    chore: Utilize calculateLabeledWitness
    chore: Update lockfile

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,28 +1,37 @@
-require('hardhat-circom');
+require("hardhat-circom");
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.6.7",
+  solidity: {
+    compilers: [
+      {
+        version: "0.6.11",
+      },
+      {
+        version: "0.8.9",
+      },
+    ],
+  },
   circom: {
     inputBasePath: "./circuits",
     ptau: "https://hermezptau.blob.core.windows.net/ptau/powersOfTau28_hez_final_15.ptau",
     circuits: [
       {
-        name: "division"
+        name: "division",
         // No protocol, so it defaults to groth16
       },
       {
         name: "simple-polynomial",
         // Generate PLONK
-        protocol: 'plonk'
+        protocol: "plonk",
       },
       {
         name: "hash",
         // Explicitly generate groth16
-        protocol: "groth16"
-      }
+        protocol: "groth16",
+      },
     ],
   },
 };
