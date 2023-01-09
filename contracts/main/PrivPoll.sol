2846ca4801e815ed37e5fb3065fd4fb45a53f8e8
Darya Kaviani <daryakavi@gmail.com>
2023-01-08 17:35:11 -0800

    privpoll and semaphore contracts

---
+++ contracts/main/PrivPoll.sol
@@ -0,0 +1,124 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+
+import "../interfaces/IPrivPoll.sol";
+import "../base/SemaphoreCore.sol";
+import "../base/SemaphoreGroups.sol";
+
+/// @title Semaphore voting contract.
+/// @dev The following code allows you to create polls, add voters and allow them to vote anonymously.
+contract SemaphoreVoting is IPrivPoll, SemaphoreCore, SemaphoreGroups {
+    /// @dev Gets a tree depth and returns its verifier address.
+    mapping(uint256 => IVerifier) internal verifiers;
+
+    /// @dev Gets a poll id and returns the poll data.
+    mapping(uint256 => Poll) internal polls;
+
+    /// @dev Gets a nullifier hash and returns true or false.
+    /// It is used to prevent double-voting.
+    mapping(uint256 => bool) internal nullifierHashes;
+
+    /// @dev Initializes the Semaphore verifiers used to verify the user's ZK proofs.
+    /// @param _verifiers: List of Semaphore verifiers (address and related Merkle tree depth).
+    constructor(Verifier[] memory _verifiers) {
+        for (uint8 i = 0; i < _verifiers.length; ) {
+            verifiers[_verifiers[i].merkleTreeDepth] = IVerifier(_verifiers[i].contractAddress);
+
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+    /// @dev Checks if the poll coordinator is the transaction sender.
+    /// @param pollId: Id of the poll.
+    modifier onlyCoordinator(uint256 pollId) {
+        if (polls[pollId].coordinator != _msgSender()) {
+            revert Semaphore__CallerIsNotThePollCoordinator();
+        }
+
+        _;
+    }
+
+    /// @dev See {ISemaphoreVoting-createPoll}.
+    function createPoll(
+        uint256 pollId,
+        address coordinator,
+        uint256 merkleRoot,
+        uint256 merkleTreeDepth
+    ) public override {
+        if (address(verifiers[merkleTreeDepth]) == address(0)) {
+            revert Semaphore__MerkleTreeDepthIsNotSupported();
+        }
+
+        _createGroup(pollId, merkleRoot, merkleTreeDepth, 0);
+
+        Poll memory poll;
+
+        poll.coordinator = coordinator;
+
+        polls[pollId] = poll;
+
+        emit PollCreated(pollId, coordinator);
+    }
+
+    /// @dev See {ISemaphoreVoting-addVoter}.
+    function startPoll(uint256 pollId) public override onlyCoordinator(pollId) {
+        if (polls[pollId].state != PollState.Created) {
+            revert Semaphore__PollHasAlreadyBeenStarted();
+        }
+
+        polls[pollId].state = PollState.Ongoing;
+
+        emit PollStarted(pollId, _msgSender());
+    }
+
+    /// @dev See {ISemaphoreVoting-castVote}.
+    function castVote(
+        bytes32 vote,
+        uint256 nullifierHash,
+        uint256 pollId,
+        uint256[8] calldata proof
+    ) public override onlyCoordinator(pollId) {
+        Poll memory poll = polls[pollId];
+
+        if (poll.state != PollState.Ongoing) {
+            revert Semaphore__PollIsNotOngoing();
+        }
+
+        if (nullifierHashes[nullifierHash]) {
+            revert Semaphore__YouAreUsingTheSameNillifierTwice();
+        }
+
+        uint256 merkleTreeDepth = getMerkleTreeDepth(pollId);
+        uint256 merkleTreeRoot = getMerkleTreeRoot(pollId);
+
+        IVerifier verifier = verifiers[merkleTreeDepth];
+
+        _verifyProof(vote, merkleTreeRoot, nullifierHash, pollId, proof, verifier);
+
+        nullifierHashes[nullifierHash] = true;
+
+        if (uint(vote) & 0xfff == 1) {
+            polls[pollId].yesVotes += 1;
+        } else {
+            polls[pollId].noVotes += 1;
+        }
+
+        emit VoteAdded(pollId, vote);
+    }
+
+    /// @dev See {ISemaphoreVoting-publishDecryptionKey}.
+    function endPoll(uint256 pollId) public override onlyCoordinator(pollId) returns (bool) {
+        if (polls[pollId].state != PollState.Ongoing) {
+            revert Semaphore__PollIsNotOngoing();
+        }
+
+        polls[pollId].state = PollState.Ended;
+
+        emit PollEnded(pollId, _msgSender());
+
+        return polls[pollId].yesVotes > polls[pollId].noVotes;
+    }
+
+}
---
+++ contracts/main/Semaphore.sol
@@ -0,0 +1,177 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+
+import "../interfaces/ISemaphore.sol";
+import "../interfaces/IVerifier.sol";
+import "../base/SemaphoreCore.sol";
+import "../base/SemaphoreGroups.sol";
+
+/// @title Semaphore
+contract Semaphore is ISemaphore, SemaphoreCore, SemaphoreGroups {
+    /// @dev Gets a tree depth and returns its verifier address.
+    mapping(uint256 => IVerifier) public verifiers;
+
+    /// @dev Gets a group id and returns the group parameters.
+    mapping(uint256 => Group) public groups;
+
+    /// @dev Checks if the group admin is the transaction sender.
+    /// @param groupId: Id of the group.
+    modifier onlyGroupAdmin(uint256 groupId) {
+        if (groups[groupId].admin != _msgSender()) {
+            revert Semaphore__CallerIsNotTheGroupAdmin();
+        }
+        _;
+    }
+
+    /// @dev Checks if there is a verifier for the given tree depth.
+    /// @param merkleTreeDepth: Depth of the tree.
+    modifier onlySupportedMerkleTreeDepth(uint256 merkleTreeDepth) {
+        if (address(verifiers[merkleTreeDepth]) == address(0)) {
+            revert Semaphore__MerkleTreeDepthIsNotSupported();
+        }
+        _;
+    }
+
+    /// @dev Initializes the Semaphore verifiers used to verify the user's ZK proofs.
+    /// @param _verifiers: List of Semaphore verifiers (address and related Merkle tree depth).
+    constructor(Verifier[] memory _verifiers) {
+        for (uint8 i = 0; i < _verifiers.length; ) {
+            verifiers[_verifiers[i].merkleTreeDepth] = IVerifier(_verifiers[i].contractAddress);
+
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+    /// @dev See {ISemaphore-createGroup}.
+    function createGroup(
+        uint256 groupId, // poll id
+        uint256 merkleTreeRoot,
+        uint256 merkleTreeDepth,
+        uint256 zeroValue,
+        address admin
+    ) external override onlySupportedMerkleTreeDepth(merkleTreeDepth) {
+        _createGroup(groupId, merkleTreeRoot, merkleTreeDepth, zeroValue);
+
+        groups[groupId].admin = admin;
+        groups[groupId].merkleRootDuration = 1 hours;
+
+        emit GroupAdminUpdated(groupId, address(0), admin);
+    }
+
+    // /// @dev See {ISemaphore-createGroup}.
+    // function createGroup(
+    //     uint256 groupId,
+    //     uint256 merkleTreeDepth,
+    //     uint256 zeroValue,
+    //     address admin,
+    //     uint256 merkleTreeRootDuration
+    // ) external override onlySupportedMerkleTreeDepth(merkleTreeDepth) {
+    //     _createGroup(groupId, merkleTreeDepth, zeroValue);
+
+    //     groups[groupId].admin = admin;
+    //     groups[groupId].merkleRootDuration = merkleTreeRootDuration;
+
+    //     emit GroupAdminUpdated(groupId, address(0), admin);
+    // }
+
+    // /// @dev See {ISemaphore-updateGroupAdmin}.
+    // function updateGroupAdmin(uint256 groupId, address newAdmin) external override onlyGroupAdmin(groupId) {
+    //     groups[groupId].admin = newAdmin;
+
+    //     emit GroupAdminUpdated(groupId, _msgSender(), newAdmin);
+    // }
+
+    // /// @dev See {ISemaphore-addMember}.
+    // function addMember(uint256 groupId, uint256 identityCommitment) external override onlyGroupAdmin(groupId) {
+    //     _addMember(groupId, identityCommitment);
+
+    //     uint256 merkleTreeRoot = getMerkleTreeRoot(groupId);
+
+    //     groups[groupId].merkleRootCreationDates[merkleTreeRoot] = block.timestamp;
+    // }
+
+    // /// @dev See {ISemaphore-addMembers}.
+    // function addMembers(uint256 groupId, uint256[] calldata identityCommitments)
+    //     external
+    //     override
+    //     onlyGroupAdmin(groupId)
+    // {
+    //     for (uint8 i = 0; i < identityCommitments.length; ) {
+    //         _addMember(groupId, identityCommitments[i]);
+
+    //         unchecked {
+    //             ++i;
+    //         }
+    //     }
+
+    //     uint256 merkleTreeRoot = getMerkleTreeRoot(groupId);
+
+    //     groups[groupId].merkleRootCreationDates[merkleTreeRoot] = block.timestamp;
+    // }
+
+    // /// @dev See {ISemaphore-updateMember}.
+    // function updateMember(
+    //     uint256 groupId,
+    //     uint256 identityCommitment,
+    //     uint256 newIdentityCommitment,
+    //     uint256[] calldata proofSiblings,
+    //     uint8[] calldata proofPathIndices
+    // ) external override onlyGroupAdmin(groupId) {
+    //     _updateMember(groupId, identityCommitment, newIdentityCommitment, proofSiblings, proofPathIndices);
+    // }
+
+    // /// @dev See {ISemaphore-removeMember}.
+    // function removeMember(
+    //     uint256 groupId,
+    //     uint256 identityCommitment,
+    //     uint256[] calldata proofSiblings,
+    //     uint8[] calldata proofPathIndices
+    // ) external override onlyGroupAdmin(groupId) {
+    //     _removeMember(groupId, identityCommitment, proofSiblings, proofPathIndices);
+    // }
+
+    /// @dev See {ISemaphore-verifyProof}.
+    function verifyProof(
+        uint256 groupId,
+        uint256 merkleTreeRoot,
+        bytes32 signal,
+        uint256 nullifierHash,
+        uint256 externalNullifier,
+        uint256[8] calldata proof
+    ) external override {
+        uint256 currentMerkleTreeRoot = getMerkleTreeRoot(groupId);
+
+        if (currentMerkleTreeRoot == 0) {
+            revert Semaphore__GroupDoesNotExist();
+        }
+
+        if (merkleTreeRoot != currentMerkleTreeRoot) {
+            uint256 merkleRootCreationDate = groups[groupId].merkleRootCreationDates[merkleTreeRoot];
+            uint256 merkleRootDuration = groups[groupId].merkleRootDuration;
+
+            if (merkleRootCreationDate == 0) {
+                revert Semaphore__MerkleTreeRootIsNotPartOfTheGroup();
+            }
+
+            if (block.timestamp > merkleRootCreationDate + merkleRootDuration) {
+                revert Semaphore__MerkleTreeRootIsExpired();
+            }
+        }
+
+        if (groups[groupId].nullifierHashes[nullifierHash]) {
+            revert Semaphore__YouAreUsingTheSameNillifierTwice();
+        }
+
+        uint256 merkleTreeDepth = getMerkleTreeDepth(groupId);
+
+        IVerifier verifier = verifiers[merkleTreeDepth];
+
+        _verifyProof(signal, merkleTreeRoot, nullifierHash, externalNullifier, proof, verifier);
+
+        groups[groupId].nullifierHashes[nullifierHash] = true;
+
+        emit ProofVerified(groupId, merkleTreeRoot, nullifierHash, externalNullifier, signal);
+    }
+}
