591d055ac695eb3879d133a01fa920e222de67af
Darya Kaviani <daryakavi@gmail.com>
2023-01-13 00:02:03 -0800

    view function for polls

--- src/pages/vote/[id].tsx
+++ src/pages/vote/[id].tsx
@@ -9,6 +9,7 @@ import {
   GridItem,
   Center,
   Input,
+  Box,
 } from "@chakra-ui/react";
 import { Flex, Spacer } from "@chakra-ui/react";
 import { useState, useEffect } from "react";
@@ -17,7 +18,15 @@ import { generateProof } from "../../components/generateProof";
 import { castVote } from "../../components/castVote";
 import { useToast } from "@chakra-ui/react";
 import { useRouter } from "next/router";
-import { useWaitForTransaction } from "wagmi";
+import {
+  useContract,
+  useWaitForTransaction,
+  useSigner,
+  useContractRead,
+} from "wagmi";
+import testABI from "../../components/abi/test.json";
+import { Progress } from "@chakra-ui/react";
+import { PieChart } from "react-minimal-pie-chart";
 
 interface IPoll {
   title: string;
@@ -32,6 +41,7 @@ interface IPoll {
 }
 
 const account = getAccount();
+const SEMAPHORE_CONTRACT = process.env.NEXT_PUBLIC_GOERLI_POLL_CONTRACT;
 
 function PollDisplay() {
   const [publicKey, setPublicKey] = useState<string>("");
@@ -43,14 +53,28 @@ function PollDisplay() {
   const [proofResponse, setProofResponse] = useState<string>("");
   const [loadingProof, setLoadingProof] = useState<boolean>(false);
   const [loadingSubmitVote, setLoadingSubmitVote] = useState<boolean>(false);
+  const [yesVoteCount, setYesVoteCount] = useState<number>(0);
+  const [noVoteCount, setNoVoteCount] = useState<number>(0);
   const [txHash, setTxHash] = useState<string>("");
   const toast = useToast();
-  const router = useRouter()
-  const { id } = router.query
+  const router = useRouter();
+  const { id } = router.query;
   const { data, isError, isLoading } = useWaitForTransaction({
     hash: `0x${txHash}`,
   });
-  console.log(id)
+  console.log(id);
+
+  const {
+    data: resultData,
+    isError: isResultError,
+    isLoading: isResultLoading,
+  } = useContractRead({
+    address: SEMAPHORE_CONTRACT,
+    abi: testABI,
+    functionName: "getPollState",
+    args: [id],
+  });
+  console.log("RESULT DATA", resultData);
 
   const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     setYesSelected(e.currentTarget.textContent === "Yes" ? true : false);
@@ -70,7 +94,13 @@ function PollDisplay() {
   });
 
   useEffect(() => {
-    if (!id) return
+    if (resultData) {
+      setYesVoteCount(Number((resultData as number[])[0]));
+      console.log("yes votes", yesVoteCount);
+      setNoVoteCount(Number((resultData as number[])[1]));
+      console.log("no votes", noVoteCount);
+    }
+    if (!id) return;
     const postData = async () => {
       const body = {
         data: {
@@ -81,12 +111,12 @@ function PollDisplay() {
       const response = await fetch("/api/getPoll", {
         method: "POST",
         body: JSON.stringify(body),
-      }).then(res => res.json());
-      console.log(response)
-      setPoll(response)
+      }).then((res) => res.json());
+      console.log(response);
+      setPoll(response);
     };
-    postData()
-  }, [id])
+    postData();
+  }, [id, resultData]);
 
   const handleGenProof = async (e: React.MouseEvent<HTMLButtonElement>) => {
     if (account) {
@@ -114,10 +144,10 @@ function PollDisplay() {
           maxWidth: "90%",
         },
       });
-      console.log("Proof Details: ", proofForTx)
+      console.log("Proof Details: ", proofForTx);
       setLoadingProof(false);
       setProofResponse(proofForTx);
-      console.log("SET TO THIS PROOF RESPONSE", proofResponse)
+      console.log("SET TO THIS PROOF RESPONSE", proofResponse);
     }
   };
 
@@ -226,7 +256,6 @@ function PollDisplay() {
             onChange={(e) => setPublicKey(e.target.value)}
           />
           <Spacer />
-
           <Input
             mr={4}
             mb={5}
@@ -259,7 +288,11 @@ function PollDisplay() {
               <Button
                 mb={10}
                 ml={4}
-                disabled={account && proofResponse == "" && (yesSelected || noSelected) ? false : true}
+                disabled={
+                  account && proofResponse == "" && (yesSelected || noSelected)
+                    ? false
+                    : true
+                }
                 onClick={handleGenProof}
                 loadingText="Generating Proof"
                 isLoading={loadingProof}
@@ -282,9 +315,43 @@ function PollDisplay() {
               </Button>
             </Flex>
           </Center>
-          <Spacer />
-          <Center></Center>
-          <Center></Center>
+          <GridItem area={"main"}>
+            <Center>
+              <Flex>
+                <Box w="40%" p={4} color="white">
+                  {yesVoteCount + noVoteCount > 0 ? (
+                    <PieChart
+                      data={[
+                        { title: "Yes", value: yesVoteCount, color: "green" },
+                        { title: "No", value: noVoteCount, color: "red" },
+                      ]}
+                    />
+                  ) : null}
+                </Box>
+                <Text
+                  color="gray.500"
+                  fontWeight="semibold"
+                  letterSpacing="wide"
+                  fontSize="xs"
+                  textTransform="uppercase"
+                  ml="2"
+                >
+                  Yes Votes: {yesVoteCount}
+                </Text>
+                <Text
+                  color="gray.500"
+                  fontWeight="semibold"
+                  letterSpacing="wide"
+                  fontSize="xs"
+                  textTransform="uppercase"
+                  ml="2"
+                >
+                  No Votes: {noVoteCount}
+                </Text>
+              </Flex>
+            </Center>
+          </GridItem>
+          <Spacer />;
         </GridItem>
       </Grid>
     </Card>
