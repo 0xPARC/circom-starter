260cd3b2562b913af975118f07270673e88bde49
ratankaliani <ratankaliani@berkeley.edu>
2023-01-13 23:03:58 -0800

    feat: END TO END ECDSA (CLIENT GENERATION)

--- frontend/components/generateProof.ts
+++ frontend/components/generateProof.ts
@@ -1,6 +1,27 @@
 import axios from "axios";
 // const snarkjs = require("snarkjs");
 // TODO: Change the storage of the Merkle Tree to an S3 Bucket
+
+import { downloadFromFilename, fullProve } from '../lib/zkp'
+
+const splitPrivateKey = (bigint_identityNullifier: bigint) => {
+    const shift = BigInt(2 ** 64);
+    const shift2 = shift * shift;
+    const shift3 = shift2 * shift;
+  
+    const chunk1 = bigint_identityNullifier % shift;
+    const chunk2 = ((bigint_identityNullifier - chunk1) / shift) % shift;
+    const chunk3 =
+      ((bigint_identityNullifier - chunk1 - chunk2 * shift) / shift2) % shift;
+    const chunk4 =
+      ((bigint_identityNullifier - chunk1 - chunk2 * shift - chunk3 * shift2) /
+        shift3) %
+      shift;
+  
+    let chunks = [chunk1, chunk2, chunk3, chunk4];
+    return chunks;
+  };
+
 async function generateProof(identityNullifier: string, publicKey: string, vote: number, pollId: number) {
     console.log("Generating Proof")
     console.log("Address: ", publicKey)
@@ -21,8 +42,11 @@ async function generateProof(identityNullifier: string, publicKey: string, vote:
     const siblings = response.data.siblings;
     const pathIndices = response.data.pathIndices;
 
+    const splitIdentityNullifier = splitPrivateKey(BigInt(identityNullifier));
+
     const input = {
         identityNullifier: identityNullifier,
+        splitIdentityNullifier: splitIdentityNullifier,
         treePathIndices: pathIndices,
         treeSiblings: siblings,
         signalHash: vote.toString(),
@@ -33,12 +57,27 @@ async function generateProof(identityNullifier: string, publicKey: string, vote:
     // const zkeyPath = "./semaphore.zkey";
     // const vkeyPath = "./semaphore.vkey.json";
 
-    const outputResponse = await axios.post("/api/generateProof", {
-        data: input
-    })
+    await downloadFromFilename("https://zkpoll.s3.us-west-1.amazonaws.com/", "ecdsa-semaphore_16");
 
-    const proofForTx = outputResponse.data.proofForTx;
-    const nullifierHash = outputResponse.data.nullifierHash;
+    const {proof, publicSignals} = await fullProve(input, "ecdsa-semaphore_16")
+    const proofForTx = [
+        proof.pi_a[0],
+        proof.pi_a[1],
+        proof.pi_b[0][1],
+        proof.pi_b[0][0],
+        proof.pi_b[1][1],
+        proof.pi_b[1][0],
+        proof.pi_c[0],
+        proof.pi_c[1],
+      ];
+    const nullifierHash = publicSignals[1];
+
+    // const outputResponse = await axios.post("/api/generateProof", {
+    //     data: input
+    // })
+
+    // const proofForTx = outputResponse.data.proofForTx;
+    // const nullifierHash = publicSignals[1];
     // console.log("In components", outputResponse.data.name)
     return ["", proofForTx, nullifierHash];
 
