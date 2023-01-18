cd808e8d855df9df0f30971d2892ef73c3868c4e
mcantillon21 <60069300+mcantillon21@users.noreply.github.com>
2023-01-18 11:18:51 -0800

    added button, curretly no submit functionality

--- frontend/pages/generatepoll.tsx
+++ frontend/pages/generatepoll.tsx
@@ -1,10 +1,10 @@
-import * as React from "react";
-import styles from "../styles/Home.module.css";
-import { useState } from "react";
-import Header from "../components/header";
-import testABI from "../components/abi/test.json";
-import { generatePollinDB } from "../components/generatePoll";
-import { getAccount } from "@wagmi/core";
+import * as React from 'react'
+import styles from '../styles/Home.module.css'
+import { useState } from 'react'
+import Header from '../components/header'
+import testABI from '../components/abi/test.json'
+import { generatePollinDB } from '../components/generatePoll'
+import { getAccount } from '@wagmi/core'
 import {
   Grid,
   GridItem,
@@ -15,84 +15,106 @@ import {
   Tooltip,
   useToast,
   Text,
-} from "@chakra-ui/react";
-import { FormControl, Input, Button, Heading } from "@chakra-ui/react";
-import { Card, CardBody } from "@chakra-ui/react";
-import { useContract, useSigner, useWaitForTransaction, useEnsAddress, createClient } from "wagmi";
-import { ethers, getDefaultProvider, Wallet} from "ethers";
+} from '@chakra-ui/react'
+import { FormControl, Input, Button, Heading } from '@chakra-ui/react'
+import { Card, CardBody } from '@chakra-ui/react'
+import {
+  useContract,
+  useSigner,
+  useWaitForTransaction,
+  useEnsAddress,
+  createClient,
+} from 'wagmi'
+import { ethers, getDefaultProvider, Wallet } from 'ethers'
 
-const provider = new ethers.providers.JsonRpcProvider("https://eth-goerli.g.alchemy.com/v2/3daFopiKN1n8gyTPl7UvqO4e0k8jETYe", "goerli");
-const wallet = new ethers.Wallet("0xfaf3aebf380aedc47d8d11acde148b1048dc123d90b0b1016faf92b7d1583a15", provider);
-const signer = wallet.connect(provider);
+import {
+  FormLabel,
+  InputGroup,
+  InputLeftElement,
+  FormErrorMessage,
+  Code,
+  Icon,
+} from '@chakra-ui/react'
+import { FiFile } from 'react-icons/fi'
 
-  const getENS = (addr: String) => {
-    addr = addr.trim();
-    const address = signer.resolveName(addr?.toString());
-    return address;
-  }
 
+const provider = new ethers.providers.JsonRpcProvider(
+  'https://eth-goerli.g.alchemy.com/v2/3daFopiKN1n8gyTPl7UvqO4e0k8jETYe',
+  'goerli',
+)
+const wallet = new ethers.Wallet(
+  '0xfaf3aebf380aedc47d8d11acde148b1048dc123d90b0b1016faf92b7d1583a15',
+  provider,
+)
+const signer = wallet.connect(provider)
+
+const getENS = (addr: String) => {
+  addr = addr.trim()
+  const address = signer.resolveName(addr?.toString())
+  return address
+}
 
 interface FormValues {
-  title: string;
-  addresses: string[];
-  description: string;
-  groupDescription: string;
-  createdAt: number;
-  deadline: number;
+  title: string
+  addresses: string[]
+  description: string
+  groupDescription: string
+  createdAt: number
+  deadline: number
 }
 
 let myResponse: {
-  name: "";
-  rootHash: "";
-  pollId: 0;
-  title: "";
-  deadline: 0;
+  name: ''
+  rootHash: ''
+  pollId: 0
+  title: ''
+  deadline: 0
 } = {
-  name: "",
-  rootHash: "",
+  name: '',
+  rootHash: '',
   pollId: 0,
-  title: "",
+  title: '',
   deadline: 0,
-};
+}
 
-const SEMAPHORE_CONTRACT = process.env.NEXT_PUBLIC_GOERLI_POLL_CONTRACT;
+const SEMAPHORE_CONTRACT = process.env.NEXT_PUBLIC_GOERLI_POLL_CONTRACT
 
 // Generate a form poll that allows a user to enter FormValues and upload a .csv
 export default function GeneratePoll() {
-  const [title, setTitle] = useState<string>("");
-  const [addresses, setAddresses] = useState<string[]>([]);
-  const [description, setDescription] = useState<string>("");
-  const [groupDescription, setGroupDescription] = useState<string>("");
-  const [duration, setDuration] = useState<number>(5);
-  const [tempAddresses, setTempAddresses] = useState<string>("");
+  const [title, setTitle] = useState<string>('')
+  const [addresses, setAddresses] = useState<string[]>([])
+  const [description, setDescription] = useState<string>('')
+  const [groupDescription, setGroupDescription] = useState<string>('')
+  const [duration, setDuration] = useState<number>(5)
+  const [tempAddresses, setTempAddresses] = useState<string>('')
   // const [pollId, setPollId] = useState<number>(1);
-  const account = getAccount();
-  const [dbLoading, setDbLoading] = useState(false);
-  const [contractLoading, setContractLoading] = useState(false);
-  const { data: signer } = useSigner();
-  const toast = useToast();
-  const [currHash, setHash] = useState();
-  const [showTooltip, setShowTooltip] = React.useState(false);
-  const [temp, setTemp] = useState<string>("");
+  const account = getAccount()
+  const [dbLoading, setDbLoading] = useState(false)
+  const [contractLoading, setContractLoading] = useState(false)
+  const { data: signer } = useSigner()
+  const toast = useToast()
+  const [currHash, setHash] = useState()
+  const [showTooltip, setShowTooltip] = React.useState(false)
+  const [temp, setTemp] = useState<string>('')
 
   const { data, isError, isLoading } = useWaitForTransaction({
     hash: currHash,
-  });
+  })
 
   const contract = useContract({
     address: SEMAPHORE_CONTRACT,
     abi: testABI,
     signerOrProvider: signer,
-  });
+  })
 
   const splitAddresses = async (stringAddresses: string) => {
-    setTempAddresses(stringAddresses);
-    const split = stringAddresses.split(",");
-    const addressesTemp:string[] = [];
+    setTempAddresses(stringAddresses)
+    const split = stringAddresses.split(',')
+    const addressesTemp: string[] = []
     if (split) {
       for (let i = 0; i < split.length; i++) {
         let addr = split[i].trim()
-        if (addr.includes(".eth")) {
+        if (addr.includes('.eth')) {
           setTemp(addr)
           let add = await getENS(addr)
           if (add) {
@@ -107,11 +129,11 @@ export default function GeneratePoll() {
     }
     setAddresses(addressesTemp)
     console.log('Final addresses ', addresses)
-  };
+  }
 
   const postData = async (addressesArr: string[]) => {
-    let setDeadline;
-    setDeadline = Date.now() + 3600 * 1000 * duration;
+    let setDeadline
+    setDeadline = Date.now() + 3600 * 1000 * duration
     // const body = {
     //   data: {
     //     title: title,
@@ -123,8 +145,15 @@ export default function GeneratePoll() {
     //   },
     // };
 
-    const dbOutput = await generatePollinDB(title, addressesArr, description, groupDescription, Date.now(), setDeadline);
-    console.log("In pages: ", dbOutput)
+    const dbOutput = await generatePollinDB(
+      title,
+      addressesArr,
+      description,
+      groupDescription,
+      Date.now(),
+      setDeadline,
+    )
+    console.log('In pages: ', dbOutput)
     return dbOutput
     // console.log(response);
     // if (response.status === 200) {
@@ -136,148 +165,166 @@ export default function GeneratePoll() {
     // } else {
     //   console.warn("Server returned error status: " + response.status);
     // }
-  };
+  }
 
   async function handleSubmit(
     e: { preventDefault: () => void },
-    addressesArr: string[]
+    addressesArr: string[],
   ) {
-    setDbLoading(true);
-    e.preventDefault();
+    setDbLoading(true)
+    e.preventDefault()
 
-    const responseArr = await postData(addressesArr);
+    const responseArr = await postData(addressesArr)
     // console.log(responseArr)
     // const pollId = responseArr[1];
     // const rootHash = responseArr[0];
-    setDbLoading(false);
-    console.log("OK ROOT HASH", responseArr.rootHash);
-    setContractLoading(true);
+    setDbLoading(false)
+    console.log('OK ROOT HASH', responseArr.rootHash)
+    setContractLoading(true)
     const tx = await contract?.createPoll(
       // 1,
       responseArr.pollId,
       account.address,
       responseArr.rootHash,
-      16
-    );
-    const response = await tx.wait();
-    setHash(tx.hash);
-    console.log("tx", tx.hash);
+      16,
+    )
+    const response = await tx.wait()
+    setHash(tx.hash)
+    console.log('tx', tx.hash)
     if (!isError) {
       toast({
-        title: "Poll created",
+        title: 'Poll created',
         description: tx.hash,
-        status: "success",
+        status: 'success',
         duration: 5000,
         isClosable: true,
         containerStyle: {
           // width: "700px",
-          maxWidth: "90%",
+          maxWidth: '90%',
         },
-      });
+      })
     } else {
       toast({
-        title: "Transaction failed",
+        title: 'Transaction failed',
         description: tx.hash,
-        status: "error",
+        status: 'error',
         duration: 5000,
         isClosable: true,
         containerStyle: {
-          width: "700px",
-          maxWidth: "90%",
+          width: '700px',
+          maxWidth: '90%',
         },
-      });
+      })
     }
-    setContractLoading(false);
-    console.log(`Transaction response: `, response);
+    setContractLoading(false)
+    console.log(`Transaction response: `, response)
   }
 
   return (
     <div className={styles.container}>
       <Header />
-        <main className={styles.main}>
-          <Card variant={"elevated"} style={{ maxWidth: "98%", width: 600}}>
-            <CardBody>
-              <FormControl
-                className={styles.generate}
-                onSubmit={(e) => handleSubmit(e, addresses)}
-              >
-                <Input
-                  placeholder="Title"
-                  value={title}
-                  onChange={(e) => setTitle(e.target.value)}
-                  focusBorderColor={"#9B72F2"}
-                />
-                <Input
-                  placeholder="Additional Description"
-                  value={description}
-                  onChange={(e) => setDescription(e.target.value)}
-                  focusBorderColor={"#9B72F2"}
-                />
-                <Input
-                  placeholder="Group Description"
-                  value={groupDescription}
-                  onChange={(e) => setGroupDescription(e.target.value)}
-                  focusBorderColor={"#9B72F2"}
-                />
-                <Input
-                  placeholder="Public Addresses"
-                  value={tempAddresses}
-                  onChange={(e) => splitAddresses(e.target.value)}
-                  focusBorderColor={"#9B72F2"}
-                />
-                <Grid templateColumns="repeat(10, 1fr)" gap={3} ml={1}>
-                  <GridItem colSpan={7} w="100%" h="10">
-                    {" "}
-                    <Slider
-                      id="slider"
-                      defaultValue={5}
-                      min={0}
-                      max={24}
-                      mt={3}
-                      colorScheme="purple"
-                      onChange={(v) => setDuration(v)}
-                      onMouseEnter={() => setShowTooltip(true)}
-                      onMouseLeave={() => setShowTooltip(false)}
+      <main className={styles.main}>
+        <Card variant={'elevated'} style={{ maxWidth: '98%', width: 600 }}>
+          <CardBody>
+            <FormControl
+              className={styles.generate}
+              onSubmit={(e) => handleSubmit(e, addresses)}
+            >
+              <Input
+                placeholder="Title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                focusBorderColor={'#9B72F2'}
+              />
+              <Input
+                placeholder="Additional Description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                focusBorderColor={'#9B72F2'}
+              />
+              <Input
+                placeholder="Group Description"
+                value={groupDescription}
+                onChange={(e) => setGroupDescription(e.target.value)}
+                focusBorderColor={'#9B72F2'}
+              />
+              <Input
+                placeholder="Public Addresses"
+                value={tempAddresses}
+                onChange={(e) => splitAddresses(e.target.value)}
+                focusBorderColor={'#9B72F2'}
+              />
+
+
+              <p>or Upload a CSV of public addresses</p>
+              <input type="file" accept={'.csv'}/>
+
+              <Grid templateColumns="repeat(10, 1fr)" gap={3} ml={1}>
+                <GridItem colSpan={7} w="100%" h="10">
+                  {' '}
+                  <Slider
+                    id="slider"
+                    defaultValue={5}
+                    min={0}
+                    max={24}
+                    mt={3}
+                    colorScheme="purple"
+                    onChange={(v) => setDuration(v)}
+                    onMouseEnter={() => setShowTooltip(true)}
+                    onMouseLeave={() => setShowTooltip(false)}
+                  >
+                    <SliderTrack>
+                      <SliderFilledTrack />
+                    </SliderTrack>
+                    <Tooltip
+                      hasArrow
+                      bg="#9B72F2"
+                      color="white"
+                      placement="top"
+                      isOpen={showTooltip}
+                      label={duration}
                     >
-                      <SliderTrack>
-                        <SliderFilledTrack />
-                      </SliderTrack>
-                      <Tooltip
-                        hasArrow
-                        bg="#9B72F2"
-                        color="white"
-                        placement="top"
-                        isOpen={showTooltip}
-                        label={duration}
-                      >
-                        <SliderThumb />
-                      </Tooltip>
-                    </Slider>
-                  </GridItem>
-                  <GridItem colSpan={3} w="100%" h="10" mt={1}>
-                  {"Duration: " + duration + " hr"}
-                  </GridItem>
-                </Grid>
-                <Button
-                  type="submit"
-                  size="md"
-                  colorScheme="blue"
-                  isLoading={contractLoading || dbLoading}
-                  loadingText={
-                    dbLoading ? "Generating merkle root" : "Submitting poll"
-                  }
-                  onClick={(e) => handleSubmit(e, addresses)}
-                  backgroundColor={"#8f00ff"}
-                  _hover={{ backgroundColor: "#5b0a91" }}
-                  color={"white"}
-                  disabled={!account.isConnected}
-                >
-                  Generate
-                </Button>
-              </FormControl>
-            </CardBody>
-          </Card>
-        </main>
+                      <SliderThumb />
+                    </Tooltip>
+                  </Slider>
+                </GridItem>
+                <GridItem colSpan={3} w="100%" h="10" mt={1}>
+                  {'Duration: ' + duration + ' hr'}
+                </GridItem>
+              </Grid>
+
+
+
+
+              <Button
+                type="submit"
+                size="md"
+                colorScheme="blue"
+                isLoading={contractLoading || dbLoading}
+                loadingText={
+                  dbLoading ? 'Generating merkle root' : 'Submitting poll'
+                }
+                onClick={(e) => handleSubmit(e, addresses)}
+                backgroundColor={'#8f00ff'}
+                _hover={{ backgroundColor: '#5b0a91' }}
+                color={'white'}
+                disabled={!account.isConnected}
+              >
+                Generate
+              </Button>
+              {/* <FileUpload
+                name="avatar"
+                acceptedFileTypes="image/*"
+                isRequired={true}
+                placeholder="Your avatar"
+                control={''}
+              >
+                New avatar
+              </FileUpload> */}
+            </FormControl>
+          </CardBody>
+        </Card>
+      </main>
     </div>
-  );
+  )
 }
