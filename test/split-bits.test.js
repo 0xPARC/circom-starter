1ab96ce5a122a8c91dd70a06e72befffc8db2909
Janabel Xia <janabeltxia@gmail.com>
2023-01-21 16:18:44 -0500

    fixed bugs in ecdsa-semaphore circuit

--- test/split-bits.test.js
+++ test/split-bits.test.js
@@ -35,3 +35,40 @@ describe("split-bits test", () => {
     assert.propertyVal(witness, "main.chunks[3]", "1");
   });
 });
+const hre = require("hardhat");
+const { assert } = require("chai");
+
+//
+describe("split-bits test", () => {
+  let circuit;
+
+  const sampleInput = {
+    in: "15",
+  };
+  const sanityCheck = true;
+
+  // before(async () => {
+  //   circuit = await hre.circuitTest.setup("split-bits");
+  // });
+
+  it("setting up circuit", async () => {
+    circuit = await hre.circuitTest.setup("split-bits");
+  });
+
+  it("produces a witness with valid constraints", async () => {
+    const witness = await circuit.calculateWitness(sampleInput, sanityCheck);
+    await circuit.checkConstraints(witness);
+  });
+
+  it("has expected witness values", async () => {
+    const witness = await circuit.calculateLabeledWitness(
+      sampleInput,
+      sanityCheck
+    );
+
+    assert.propertyVal(witness, "main.chunks[0]", "1");
+    assert.propertyVal(witness, "main.chunks[1]", "1");
+    assert.propertyVal(witness, "main.chunks[2]", "1");
+    assert.propertyVal(witness, "main.chunks[3]", "1");
+  });
+});
